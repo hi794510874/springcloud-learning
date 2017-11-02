6ed1ae16a841182f01ae8afb4b25d3a396fdb769
huangb黄波 <huang_b@ctrip.com>
2017-11-02 15:06:41 +0800

    listener eureka 事件

---
+++ service-provider/src/main/java/com/owen/Listener/EurekaInstanListener.java
@@ -0,0 +1,64 @@
+package com.owen.Listener;
+
+import com.netflix.discovery.shared.Applications;
+import com.netflix.eureka.EurekaServerContextHolder;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.discovery.event.HeartbeatEvent;
+import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaRegistryAvailableEvent;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaAutoServiceRegistration;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 用于监听 eureka 服务的注册 注销 等操作
+ * Created by huang_b on 2017/9/21.
+ */
+@Configuration
+@EnableScheduling
+public class EurekaInstanListener implements ApplicationListener {
+    private Logger log = LoggerFactory.getLogger(EurekaInstanListener.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+
+        log.info("监听到的事件:" + applicationEvent.toString());
+        // 服务挂掉事件
+        if (applicationEvent instanceof EurekaInstanceCanceledEvent) {
+            EurekaInstanceCanceledEvent event = (EurekaInstanceCanceledEvent) applicationEvent;
+            // 获取当前Eureka实例中的节点信息
+            PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
+            Applications applications = registry.getApplications();
+            // 遍历获取已注册节点中与当前失效节点ID一致的节点信息
+            applications.getRegisteredApplications().forEach((registeredApplication) -> {
+                registeredApplication.getInstances().forEach((instance) -> {
+                    if (instance.getInstanceId().equals(event.getServerId())) {
+                        log.info("服务：" + instance.getAppName() + " 挂啦。。。");
+                        // // TODO: 2017/9/3 扩展消息提醒 邮件、手机短信、微信等
+                    }
+                });
+            });
+        }
+        if (applicationEvent instanceof InstanceRegisteredEvent) {
+            InstanceRegisteredEvent event = (InstanceRegisteredEvent) applicationEvent;
+            EurekaInstanceConfigBean eurekaInstanceConfigBean=(EurekaInstanceConfigBean)event.getConfig();
+            EurekaAutoServiceRegistration eurekaAutoServiceRegistration=(EurekaAutoServiceRegistration)event.getSource();
+            log.info("服务：" + eurekaInstanceConfigBean.getAppname() + " 注册成功啦。。。");
+        }
+
+        if (applicationEvent instanceof HeartbeatEvent) {
+            HeartbeatEvent event = (HeartbeatEvent) applicationEvent;
+            log.info("心跳检测服务：" + event.getValue());
+        }
+
+        if (applicationEvent instanceof EurekaRegistryAvailableEvent) {
+            log.info("服务 Aualiable。。");
+        }
+    }
+}
