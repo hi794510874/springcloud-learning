ba34e5157dece0b734d91a610e8a7566993c39bd
huangb黄波 <huang_b@ctrip.com>
2017-11-24 10:29:01 +0800

    services-ribbon -> services-provider 的调用用zipkin 记录下来

--- config-client/src/main/java/com/owen/Controller/ReadConfig.java
+++ config-client/src/main/java/com/owen/Controller/ReadConfig.java
@@ -1,6 +1,7 @@
 package com.owen.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- zipkin-server/src/main/java/com/owen/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/owen/ZipkinServerApplication.java
@@ -2,11 +2,15 @@ package com.owen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
+@EnableEurekaClient
 public class ZipkinServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 }
