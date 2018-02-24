f95b30b752b307a2ef97e644b9105c4806300087
huangb黄波 <huang_b@ctrip.com>
2018-02-24 15:46:10 +0800

    add

---
+++ kafka-consumer/src/main/java/com/owen/App.java
@@ -0,0 +1,13 @@
+package com.owen;
+
+/**
+ * 为收集日志而生，高吞吐 性能彪悍
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
